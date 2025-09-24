1f547873d901662dd1d3594eabfa17a459c91f05
Puntawat Subhamani <puntawatsub@gmail.com>
2025-09-24 15:39:00 +0300

    test code

--- simu/model/MyEngine.java
+++ simu/model/MyEngine.java
@@ -4,6 +4,11 @@ import eduni.distributions.*;
 import simu.framework.*;
 import simu.model.bEvent.BEvent;
 import simu.model.servicePoints.CustomerServicePoint;
+import simu.model.servicePoints.InspectionServicePoint;
+import simu.model.servicePoints.MaintenanceServicePoint;
+import simu.model.servicePoints.maintenanceStations.OilChangeServicePoint;
+import simu.model.servicePoints.maintenanceStations.OtherServicePoint;
+import simu.model.servicePoints.maintenanceStations.TireChangeServicePoint;
 
 import java.util.Random;
 
@@ -31,29 +36,88 @@ public class MyEngine extends Engine {
 	 */
 
     public MyEngine() {
-        // TODO: use an appropriate ContinuousGenerator (you can choose one you think it would fit)
-        // TODO: replace with a continuous generator
-        cGenerator = new ContinuousGenerator();
+//        // TODO: use an appropriate ContinuousGenerator (you can choose one you think it would fit)
+//        // TODO: replace with a continuous generator
+//        cGenerator = new ContinuousGenerator();
+//
+//        // TODO: Create service points
+//        servicePoints = new ServicePoint[6];
+//
+//        /* TODO: There are six service points
+//           0. Customer service
+//           1. Maintenance
+//           2. Tire Change
+//           3. Oil Change
+//           4. Other repairs
+//           5. Inspection
+//
+//           Each service points has its own class
+//           Service points should be added to the array in this order
+//         */
+//        // EXAMPLE:
+//        servicePoints[0] = new CustomerServicePoint(cGenerator, eventList);
+//
+//        // TODO: also create an arrival process
+//        arrivalProcess = new ArrivalProcess(cGenerator, eventList, EventType.ARR_CUSTOMER_SERVICE);
 
-        // TODO: Create service points
+        test();
+
+    }
+
+    private void test() {
+        cGenerator = new ContinuousGenerator() {
+            @Override
+            public double sample() {
+                return 10;
+            }
+
+            @Override
+            public void setSeed(long seed) {
+
+            }
+
+            @Override
+            public long getSeed() {
+                return 10;
+            }
+
+            @Override
+            public void reseed() {
+
+            }
+        };
+
         servicePoints = new ServicePoint[6];
 
-        /* TODO: There are six service points
-           0. Customer service
-           1. Maintenance
-           2. Tire Change
-           3. Oil Change
-           4. Other repairs
-           5. Inspection
-
-           Each service points has its own class
-           Service points should be added to the array in this order
-         */
-        // EXAMPLE:
         servicePoints[0] = new CustomerServicePoint(cGenerator, eventList);
+        servicePoints[1] = new MaintenanceServicePoint(cGenerator, eventList);
+        servicePoints[2] = new TireChangeServicePoint(cGenerator, eventList);
+        servicePoints[3] = new OilChangeServicePoint(cGenerator, eventList);
+        servicePoints[4] = new OtherServicePoint(cGenerator, eventList);
+        servicePoints[5] = new InspectionServicePoint(cGenerator, eventList);
 
-        // TODO: also create an arrival process
-        arrivalProcess = new ArrivalProcess(cGenerator, eventList, EventType.ARR_CUSTOMER_SERVICE);
+        arrivalProcess = new ArrivalProcess(new ContinuousGenerator() {
+            @Override
+            public double sample() {
+                return 20;
+            }
+
+            @Override
+            public void setSeed(long seed) {
+
+            }
+
+            @Override
+            public long getSeed() {
+                return 0;
+            }
+
+            @Override
+            public void reseed() {
+
+            }
+        }, eventList, EventType.ARR_CUSTOMER_SERVICE);
+
     }
 
 	@Override
--- simu/model/bEvent/BEvent.java
+++ simu/model/bEvent/BEvent.java
@@ -1,6 +1,8 @@
 package simu.model.bEvent;
 
+import eduni.distributions.ContinuousGenerator;
 import simu.framework.ArrivalProcess;
+import simu.framework.Clock;
 import simu.framework.Event;
 import simu.framework.IEventType;
 import simu.model.Customer;
@@ -37,36 +39,120 @@ public class BEvent {
          */
 
 
+//        Customer a;
+//
+//		switch ((EventType)event.getType()) {
+////      Example:
+////
+////		case ARR1:
+////			servicePoints[0].addQueue(new Customer());
+////			arrivalProcess.generateNextEvent();
+////			break;
+////
+////		case DEP1:
+////			a = servicePoints[0].removeQueue();
+////			servicePoints[1].addQueue(a);
+////			break;
+////
+////		case DEP2:
+////			a = servicePoints[1].removeQueue();
+////			servicePoints[2].addQueue(a);
+////			break;
+////
+////		case DEP3:
+////			a = servicePoints[2].removeQueue();
+////			a.setRemovalTime(Clock.getInstance().getClock());
+////		    a.reportResults();
+////			break;
+//
+//            // Example
+//            case ARR_CUSTOMER_SERVICE:
+//                servicePoints[0].addQueue(new Customer(generator, percentage, percentage));
+//                break;
+//		}
+        test(servicePoints, arrivalProcess, event);
+    }
+
+    static private void test(
+            ServicePoint[] servicePoints,
+            ArrivalProcess arrivalProcess,
+            Event event
+    ) {
         Customer a;
 
-		switch ((EventType)event.getType()) {
-//      Example:
-//
-//		case ARR1:
-//			servicePoints[0].addQueue(new Customer());
-//			arrivalProcess.generateNextEvent();
-//			break;
-//
-//		case DEP1:
-//			a = servicePoints[0].removeQueue();
-//			servicePoints[1].addQueue(a);
-//			break;
-//
-//		case DEP2:
-//			a = servicePoints[1].removeQueue();
-//			servicePoints[2].addQueue(a);
-//			break;
-//
-//		case DEP3:
-//			a = servicePoints[2].removeQueue();
-//			a.setRemovalTime(Clock.getInstance().getClock());
-//		    a.reportResults();
-//			break;
-
-            // Example
+        switch ((EventType) event.getType()) {
             case ARR_CUSTOMER_SERVICE:
-                servicePoints[0].addQueue(new Customer(generator, percentage, percentage));
+                servicePoints[0].addQueue(new Customer(new ContinuousGenerator() {
+                    @Override
+                    public double sample() {
+                        return 10;
+                    }
+
+                    @Override
+                    public void setSeed(long seed) {
+
+                    }
+
+                    @Override
+                    public long getSeed() {
+                        return 0;
+                    }
+
+                    @Override
+                    public void reseed() {
+
+                    }
+                }, 1, 1));
+                arrivalProcess.generateNextEvent();
                 break;
-		}
+
+                /* There are six service points
+           0. Customer service
+           1. Maintenance
+           2. Tire Change
+           3. Oil Change
+           4. Other repairs
+           5. Inspection
+
+           Each service points are in the array servicePoints[], in this exact order
+         */
+            case DEP_CS_INSPECTION:
+                a = servicePoints[0].removeQueue();
+                servicePoints[5].addQueue(a);
+                break;
+            case DEP_INSPECTION_MAINTENANCE:
+                a = servicePoints[5].removeQueue();
+                servicePoints[1].addQueue(a);
+                break;
+            case DEP_MAINTENANCE_OIL:
+                a = servicePoints[1].removeQueue();
+                servicePoints[3].addQueue(a);
+                break;
+            case DEP_MAINTENANCE_OTHER:
+                a = servicePoints[1].removeQueue();
+                servicePoints[4].addQueue(a);
+                break;
+            case DEP_MAINTENANCE_TIRE:
+                a = servicePoints[1].removeQueue();
+                servicePoints[2].addQueue(a);
+                break;
+            case DEP_OIL_INSPECTION:
+                a = servicePoints[3].removeQueue();
+                servicePoints[5].addQueue(a);
+                break;
+            case DEP_TIRE_INSPECTION:
+                a = servicePoints[2].removeQueue();
+                servicePoints[5].addQueue(a);
+                break;
+            case DEP_OTHER_INSPECTION:
+                a = servicePoints[3].removeQueue();
+                servicePoints[5].addQueue(a);
+                break;
+            case DEP_INSPECTION_END:
+                a = servicePoints[5].removeQueue();
+                a.setRemovalTime(Clock.getInstance().getClock());
+                a.reportResults();
+                break;
+        }
     }
 }
