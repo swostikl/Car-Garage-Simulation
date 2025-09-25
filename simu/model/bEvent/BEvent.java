fd97384de5ed65edf41a8fef21e169c545452b31
Chandina-Nilukshi <chandinn@metropolia.fi>
2025-09-25 17:07:50 +0300

    B-phase branch changes

--- simu/model/bEvent/BEvent.java
+++ simu/model/bEvent/BEvent.java
@@ -1,6 +1,10 @@
 package simu.model.bEvent;
 
+import eduni.distributions.ContinuousGenerator;
+import eduni.distributions.Generator;
+import eduni.distributions.Normal;
 import simu.framework.ArrivalProcess;
+import simu.framework.Clock;
 import simu.framework.Event;
 import simu.framework.IEventType;
 import simu.model.Customer;
@@ -64,8 +68,55 @@ public class BEvent {
 
             // Example
             case ARR_CUSTOMER_SERVICE:
-                servicePoints[0].addQueue(new Customer(generator, percentage, percentage));
+                ContinuousGenerator maintenanceGenerator = new Normal(2, 1);
+                a = new Customer(maintenanceGenerator, 0.6, 0.1);
+                servicePoints[0].addQueue(a);
 				arrivalProcess.generateNextEvent();
+
+                if (!servicePoints[0].isOnQueue()){
+                    servicePoints[0].beginService();
+                }
+                break;
+
+            case DEP_CS_INSPECTION:
+                a = servicePoints[0].removeQueue();
+                servicePoints[1].addQueue(a);
+                if (!servicePoints[1].isOnQueue()){
+                    servicePoints[1].beginService();
+                }
+                break;
+            case DEP_CS_MAINTENANCE:
+                a = servicePoints[1].removeQueue();
+                servicePoints[2].addQueue(a);
+                if (!servicePoints[2].isOnQueue()){
+                    servicePoints[2].beginService();
+                }
+                break;
+            case DEP_TIRE_MAINTENANCE:
+                a = servicePoints[2].removeQueue();
+                servicePoints[3].addQueue(a);
+                if (!servicePoints[3].isOnQueue()){
+                    servicePoints[3].beginService();
+                }
+                break;
+            case DEP_OIL_MAINTENANCE:
+                a = servicePoints[3].removeQueue();
+                servicePoints[4].addQueue(a);
+                if (!servicePoints[4].isOnQueue()){
+                    servicePoints[4].beginService();
+                }
+                break;
+            case DEP_OTHER_MAINTENANCE:
+                a = servicePoints[4].removeQueue();
+                servicePoints[5].addQueue(a);
+                if (!servicePoints[5].isOnQueue()){
+                    servicePoints[5].beginService();
+                }
+                break;
+            case DEP_INSPECTION_END:
+                a = servicePoints[5].removeQueue();
+                a.setRemovalTime(Clock.getInstance().getClock());
+                a.reportResults();
                 break;
 		}
     }
