98a8e2ab7e2924abad4679b15d97644348b4dac9
Swostika lama <swostikl@metropolia.fi>
2025-09-30 14:38:08 +0300

    Add VisualizeController and update visualize_view.fxml with new UI elements

---
+++ src/main/java/controller/VisualizeController.java
@@ -0,0 +1,85 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class VisualizeController {
+
+    @FXML
+    private Rectangle arrivalQueue;
+
+    @FXML
+    private Rectangle customerServed;
+
+    @FXML
+    private Label customerServedlabel;
+
+    @FXML
+    private Group customerService;
+
+    @FXML
+    private Label customerServicelabel;
+
+    @FXML
+    private Group inspectionQueue;
+
+    @FXML
+    private Label inspectionQueuelabel;
+
+    @FXML
+    private Rectangle inspectionService;
+
+    @FXML
+    private Label inspectionServicelabel;
+
+    @FXML
+    private Group maintenanceQueue;
+
+    @FXML
+    private Label maintenanceQueuelabel;
+
+    @FXML
+    private Rectangle maintenanceService;
+
+    @FXML
+    private Label maintenancelabel;
+
+    @FXML
+    private Group oilChangeQueue;
+
+    @FXML
+    private Label oilChangeQueuelabel;
+
+    @FXML
+    private Rectangle oilChangeService;
+
+    @FXML
+    private Label oilChangeServicelabel;
+
+    @FXML
+    private Group repairWorkQueue;
+
+    @FXML
+    private Rectangle repairWorkService;
+
+    @FXML
+    private Label repairWorklabel;
+
+    @FXML
+    private Label repairworkQueueLabel;
+
+    @FXML
+    private Group tireChangeQueue;
+
+    @FXML
+    private Label tireChangeQueuelabel;
+
+    @FXML
+    private Rectangle tireChangeService;
+
+    @FXML
+    private Label tireChangeServicelabel;
+
+}
