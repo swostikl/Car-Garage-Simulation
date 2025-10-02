b8e9ff38915f298aed7a317aac4bb7d61692bbaf
jiyajameela <jiyak@metropolia.fi>
2025-10-02 17:37:35 +0300

    .- removed run button from StepperViewController . -Added one-time run button to prevent multiple window pop-up

--- src/main/java/simu/controller/SimulatorSetupViewController.java
+++ src/main/java/simu/controller/SimulatorSetupViewController.java
@@ -1,117 +1,180 @@
+//package simu.controller;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.Slider;
+//import javafx.scene.control.TextField;
+//import javafx.scene.input.MouseEvent;
+//import javafx.stage.Stage;
+//import simu.view.StepperView;
+//
+//import java.io.IOException;
+//
+//public class SimulatorSetupViewController {
+//
+//    @FXML
+//    private Button applySimulationTime;
+//
+//    @FXML
+//    private Button arrivalApply;
+//
+//    @FXML
+//    private TextField arrivalMean;
+//
+//    @FXML
+//    private TextField arrivalVariance;
+//
+//    @FXML
+//    private Label delayLabel;
+//
+//    @FXML
+//    private Button increaseSpeed;
+//
+//    @FXML
+//    private Slider inspectionFailRate;
+//
+//    @FXML
+//    private TextField meanService;
+//
+//    @FXML
+//    private Button runProgram;
+//
+//    @FXML
+//    private Button serviceApply;
+//
+//    @FXML
+//    private TextField serviceVariance;
+//
+//    @FXML
+//    private Button setDefault;
+//
+//    @FXML
+//    private Button setSave;
+//
+//    @FXML
+//    private Button slowSpeed;
+//
+//    @FXML
+//    private TextField totalTime;
+//
+//    private StepperView stepperView;
+//
+//    @FXML
+//    void onApplySimulationTime(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onArrivalApply(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onDecreaseSpeed(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onIncreaseSpeed(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onInspectionFailRate(MouseEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onRunProgram(ActionEvent event) {
+//        stepperView = new StepperView();
+//        try {
+//            Stage stage = stepperView.init();
+//            runProgram.setDisable(true);
+//            stage.showAndWait();
+//            runProgram.setDisable(false);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @FXML
+//    void onServiceApply(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onSetDefault(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void onSetSave(ActionEvent event) {
+//
+//    }
+//
+//}
 package simu.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import simu.framework.Engine;
+import simu.framework.Trace;
+import simu.model.MyEngine;
 import simu.view.StepperView;
-
+import simu.view.VisualizeView;
 import java.io.IOException;
 
 public class SimulatorSetupViewController {
 
-    @FXML
-    private Button applySimulationTime;
+    private boolean hasRun = false;
+
+    @FXML private TextField arrivalMean;
+    @FXML private TextField arrivalVariance;
+    @FXML private TextField totalTime;
+    @FXML private TextField meanService;
+    @FXML private TextField serviceVariance;
+    @FXML private Slider inspectionFailRate;
+    @FXML private Button runProgram;
+    @FXML private Button setDefault;
+    @FXML private Button setSave;
 
     @FXML
-    private Button arrivalApply;
+    void onRunProgram(ActionEvent event) throws IOException {
+        if (!hasRun) {
 
-    @FXML
-    private TextField arrivalMean;
-
-    @FXML
-    private TextField arrivalVariance;
-
-    @FXML
-    private Label delayLabel;
-
-    @FXML
-    private Button increaseSpeed;
-
-    @FXML
-    private Slider inspectionFailRate;
-
-    @FXML
-    private TextField meanService;
-
-    @FXML
-    private Button runProgram;
-
-    @FXML
-    private Button serviceApply;
-
-    @FXML
-    private TextField serviceVariance;
-
-    @FXML
-    private Button setDefault;
-
-    @FXML
-    private Button setSave;
-
-    @FXML
-    private Button slowSpeed;
-
-    @FXML
-    private TextField totalTime;
-
-    private StepperView stepperView;
-
-    @FXML
-    void onApplySimulationTime(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onArrivalApply(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onDecreaseSpeed(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onIncreaseSpeed(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onInspectionFailRate(MouseEvent event) {
-
-    }
-
-    @FXML
-    void onRunProgram(ActionEvent event) {
-        stepperView = new StepperView();
-        try {
-            Stage stage = stepperView.init();
+            runProgram.setText("Running");
             runProgram.setDisable(true);
-            stage.showAndWait();
-            runProgram.setDisable(false);
-        } catch (IOException e) {
-            e.printStackTrace();
+            hasRun = true;
+
+            // visualization window first
+            VisualizeView visualizeView = new VisualizeView();
+            VisualizeController visualizeController = visualizeView.init();
+
+            //then stepper view
+            StepperView stepperView = new StepperView();
+            Stage stepperStage = stepperView.init();
+            stepperStage.setTitle("Simulation Control");
+
+            // to start the simulation through the stepper controller
+            StepperViewController stepperController = stepperView.getController();
+            stepperController.startSimulation(visualizeController);
+
+            System.out.println("Simulation started - Run button now disabled");
+        } else {
+            System.out.println("Simulation already running - ignoring additional clicks");
         }
     }
 
-    @FXML
-    void onServiceApply(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSetDefault(ActionEvent event) {
-
-    }
-
-    @FXML
-    void onSetSave(ActionEvent event) {
-
-    }
-
+    @FXML void onArrivalApply(ActionEvent event) { }
+    @FXML void onServiceApply(ActionEvent event) { }
+    @FXML void onApplySimulationTime(ActionEvent event) { }
+    @FXML void onInspectionFailRate() { }
+    @FXML void onSetDefault(ActionEvent event) { }
+    @FXML void onSetSave(ActionEvent event) { }
 }
--- src/main/java/simu/view/StepperView.java
+++ src/main/java/simu/view/StepperView.java
@@ -1,22 +1,56 @@
+//package simu.view;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//import simu.controller.StepperViewController;
+//
+//import java.io.IOException;
+//
+//// don't worry about the 1 related problem, it is from Unit Test
+//public class StepperView {
+//
+//    private StepperViewController controller;
+//
+//    /**
+//     * Create and display stepper view
+//     * @Note you can pass parameters here if needed
+//     * @throws IOException
+//     */
+//    public Stage init() throws IOException {
+//        Stage stage = new Stage();
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/stepper.fxml"));
+//        Parent root = loader.load();
+//        controller = loader.getController();
+//        controller.init();
+//
+//        Scene scene = new Scene(root);
+//        stage.setScene(scene);
+//        return stage;
+//    }
+//
+//    public StepperViewController getController() {
+//        return controller;
+//    }
+//}
+
 package simu.view;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import simu.controller.StepperViewController;
-
 import java.io.IOException;
 
-// don't worry about the 1 related problem, it is from Unit Test
 public class StepperView {
 
     private StepperViewController controller;
 
     /**
      * Create and display stepper view
-     * @Note you can pass parameters here if needed
      * @throws IOException
      */
     public Stage init() throws IOException {
@@ -28,6 +62,7 @@ public class StepperView {
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.show();
         return stage;
     }
 
@@ -35,4 +70,3 @@ public class StepperView {
         return controller;
     }
 }
-
