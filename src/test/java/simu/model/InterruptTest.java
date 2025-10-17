a9575eb724cb69008158822ea6795e32e346be21
Swostika lama <swostikl@metropolia.fi>
2025-10-17 06:49:08 +0300

    Add unit tests for Interrupt to verify run method calls await

---
+++ src/test/java/simu/model/InterruptTest.java
@@ -0,0 +1,22 @@
+package simu.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class InterruptTest {
+// Test that Interrupt.run() property calls await() from process
+    @Test
+    void runCallsAwait() {
+        Interrupt interrupt = spy(new Interrupt(){
+            @Override
+            public void await(){
+                // to prevent blocking
+            }
+        });
+        interrupt.run();
+        verify(interrupt, times(1)).await();
+
+
+    }
+}
